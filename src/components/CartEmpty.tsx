925e439b8cc12736adf1887b9012928a22c7ecaa
gladyshef.anton <gladyshef.anton@gmail.com>
2026-04-17 20:32:19 +0300

    Типизация onClick, onChange и Event (TypeScript)ч2

--- src/components/CartEmpty.tsx
+++ src/components/CartEmpty.tsx
@@ -2,26 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import cartEmptyImg from "../assets/img/empty-cart.png";
 
-const CartEmpty: React.FC = () => {
-  return (
-    <>
-      <div className="cart cart--empty">
-        <h2>
-          Корзина пустая <span>😕</span>
-        </h2>
-        <p>
-          Вероятней всего, вы не заказывали ещё пиццу.
-          <br />
-          Для того, чтобы заказать пиццу, перейди на главную страницу.
-        </p>
-        <img src={cartEmptyImg} className="empty-cart" alt="Empty cart" />
-        <Link to="/" className="button button--black">
-          <span>Вернуться назад</span>
-        </Link>
-      </div>
-      );
-    </>
-  );
-};
+const CartEmpty: React.FC = () => (
+  <div className="cart cart--empty">
+    <h2>
+      Корзина пустая <span>😕</span>
+    </h2>
+    <p>
+      Вероятней всего, вы не заказывали ещё пиццу.
+      <br />
+      Для того, чтобы заказать пиццу, перейди на главную страницу.
+    </p>
+    <img src={cartEmptyImg} className="empty-cart" alt="Empty cart" />
+    <Link to="/" className="button button--black">
+      <span>Вернуться назад</span>
+    </Link>
+  </div>
+);
 
 export default CartEmpty;
--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -11,20 +11,18 @@ type PaginationProps = {
 const Pagination: React.FC<PaginationProps> = ({
   currentPage,
   onChangePage,
-}) => {
-  return (
-    <ReactPaginate
-      className={styles.root}
-      breakLabel="..."
-      nextLabel=">"
-      previousLabel="<"
-      onPageChange={(event) => onChangePage(event.selected + 1)}
-      pageRangeDisplayed={4}
-      pageCount={3}
-      forcePage={currentPage - 1}
-      renderOnZeroPageCount={null}
-    />
-  );
-};
+}) => (
+  <ReactPaginate
+    className={styles.root}
+    breakLabel="..."
+    nextLabel=">"
+    previousLabel="<"
+    onPageChange={(event) => onChangePage(event.selected + 1)}
+    pageRangeDisplayed={4}
+    pageCount={3}
+    forcePage={currentPage - 1}
+    renderOnZeroPageCount={null}
+  />
+);
 
 export default Pagination;
